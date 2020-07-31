b7f0e949af35b22927a259d6f6b6443ea8ef86df
Deepjyoti De <deepjyotide13@gmail.com>
2020-08-01 02:30:25 +0530

    commit

--- static/js/tinyInject.js
+++ static/js/tinyInject.js
@@ -20,5 +20,6 @@ script.onload = function () {
         },
         menubar: 'favs file edit view insert format tools table help',
         // content_css: 'css/content.css'
+        images_upload_url: '/static/',
     });
 }
