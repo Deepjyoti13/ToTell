aade98d9e6d7c99a59798d6f3b2f13face75b2c1
Deepjyoti De <deepjyotide13@gmail.com>
2020-08-01 03:12:58 +0530

    Eighteenth

--- static/js/tinyInject.js
+++ static/js/tinyInject.js
@@ -8,18 +8,41 @@ script.onload = function () {
         width: 1200,
         height: 300,
         plugins: [
-            'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+            'advlist autolink link image imagetools lists charmap print preview hr anchor pagebreak',
             'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
             'table emoticons template paste help'
         ],
-        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+        toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
             'bullist numlist outdent indent | link image | print preview media fullpage | ' +
-            'forecolor backcolor emoticons | help',
+            'forecolor backcolor emoticons | help | image',
+        imagetools_toolbar: "rotateleft rotateright | flipv fliph | editimage imageoptions",
+        image_title: true,
+        automatic_upload: true,
+        file_picker_types: 'image',
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.onchange = function() {
+                var file = this.files[0];
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+                    cb(blobInfo.blobUri(), {title: file.name});
+                };
+                reader.readAsDataURL(file);
+            };
+            input.click();
+        },
         menu: {
             favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
         },
         menubar: 'favs file edit view insert format tools table help',
         // content_css: 'css/content.css'
-        images_upload_url: '/static/',
+        // images_upload_url: '/static/',
     });
 }
