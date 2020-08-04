31d6aa4e9988fea663167d936e0e0e14358c7388
Deepjyoti De <deepjyotide13@gmail.com>
2020-08-05 00:12:39 +0530

    twenty sixth

--- Blog/views.py
+++ Blog/views.py
@@ -9,11 +9,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-
-# Create your views here.
-# def home(request):
-#     return render(request, "Home/home.html")
-
 def home(request):
     if request.user.is_authenticated:
         if not request.user.writer.bio:
@@ -35,10 +30,9 @@ def home(request):
     else:
         r1 = recommended[:1]
         r2 = recommended[1:]
-    trending = allPosts.order_by('-view')[:30]
+    trending = allPosts.order_by('-view')[:3]
     context = {'allPosts': allPosts, 'latest1': latest1, 'latest2': latest2, 'latest3': latest3, 'recommended': recommended, 'r1': r1, 'r2': r2, 'trending': trending}
     return render(request, "Home/home.html", context)
-
 
 def registerPage(request):
     form = CreateUserForm()
@@ -51,7 +45,6 @@ def registerPage(request):
             return redirect('login')
     context = {'form': form}
     return render(request, 'Home/register.html', context)
-
 
 def loginPage(request):
     if request.method == 'POST':
@@ -69,11 +62,9 @@ def loginPage(request):
     context = {}
     return render(request, 'Home/login.html', context)
 
-
 def logoutUser(request):
     logout(request)
     return redirect('login')
-
 
 def account_details(request):
     writer = request.user.writer
@@ -89,7 +80,6 @@ def account_details(request):
     context = {'form': form}
     return render(request, 'Home/account_details.html', context)
 
-
 def accountSettings(request):
     writer = request.user.writer
     form = WriterForm(instance=writer)
@@ -100,7 +90,6 @@ def accountSettings(request):
             return redirect('/')
     context = {'form': form}
     return render(request, 'Home/account_settings.html', context)
-
 
 def writewithus(request):
     if request.user.is_authenticated:
@@ -117,9 +106,10 @@ def writewithus(request):
                 return redirect('/')
         else:
             print(form.errors)
-    context = {'form': form}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'form': form, 'recommended': recommended}
     return render(request, 'Blog/writewithus.html', context)
-
 
 def sneakpeek(request):
     sneakpeek = Post.objects.filter(approve=True, blogType='Sneak Peek')
@@ -127,9 +117,10 @@ def sneakpeek(request):
         s1 = sneakpeek[len(sneakpeek)-1]
     else:
         s1 = sneakpeek[0:1]
-    context = {'sneakpeek': sneakpeek, 's1': s1}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'sneakpeek': sneakpeek, 's1': s1, 'recommended': recommended}
     return render(request, "Blog/sneakpeek.html", context)
-
 
 def lifestyle(request):
     lifestyle = Post.objects.filter(approve=True, blogType='Lifestyle')
@@ -137,9 +128,10 @@ def lifestyle(request):
         l1 = lifestyle[len(lifestyle)-1]
     else:
         l1 = lifestyle[0:1]
-    context = {'lifestyle': lifestyle, 'l1': l1}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'lifestyle': lifestyle, 'l1': l1, 'recommended': recommended}
     return render(request, "Blog/lifestyle.html", context)
-
 
 def events(request):
     events = Post.objects.filter(approve=True, blogType='Events')
@@ -147,9 +139,10 @@ def events(request):
         e1 = events[len(events)-1]
     else:
         e1 = events[0:1]
-    context = {'events': events, 'e1': e1}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'events': events, 'e1': e1, 'recommended': recommended}
     return render(request, "Blog/events.html", context)
-
 
 def trends(request):
     trends = Post.objects.filter(approve=True, blogType='Trends')
@@ -157,9 +150,10 @@ def trends(request):
         t1 = trends[len(trends)-1]
     else:
         t1 = trends[0:1]
-    context = {'trends': trends, 't1': t1}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'trends': trends, 't1': t1, 'recommended': recommended}
     return render(request, "Blog/trends.html", context)
-
 
 def sciencetech(request):
     scitech = Post.objects.filter(
@@ -168,9 +162,10 @@ def sciencetech(request):
         t1 = scitech[len(scitech)-1]
     else:
         t1 = scitech[0:1]
-    context = {'scitech': scitech, 't1': t1}
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    context = {'scitech': scitech, 't1': t1, 'recommended': recommended}
     return render(request, "Blog/sciencetech.html", context)
-
 
 def blog(request, pk):
     post = Post.objects.get(id=pk)
@@ -180,9 +175,11 @@ def blog(request, pk):
     liked = False
     if post.likes.filter(id=request.user.id).exists():
         liked = True
-    context = {'post': post, 'comments': comments, 'liked': liked}
-    return render(request, "Blog/article.html", context)
-
+    allPosts = Post.objects.filter(approve=True)
+    recommended = allPosts.filter(recommend=True).order_by('-id')[:3]
+    trending = allPosts.order_by('-view')[:4]
+    context = {'post': post, 'comments': comments, 'liked': liked, 'trending': trending, 'recommended': recommended}
+    return render(request, "Blog/article2.html", context)
 
 def like(request, pk):
     if request.user.is_authenticated:
@@ -198,19 +195,11 @@ def like(request, pk):
         return redirect("login")
     return HttpResponseRedirect(reverse('blog', args=[str(pk)]))
 
-# def view(request, pk):
-#     if request.user.is_authenticated:
-#         post = get_object_or_404(Post, id=request.POST.get('post_id'))
-#         post.view.add(request.user)
-#     return HttpResponseRedirect(reverse('blog', args=[str(pk)]))
-
-
 def profile(request, pk):
     writer = Writer.objects.get(id=pk)
     posts = writer.tags.filter(approve=True)
     context = {'writer': writer, 'posts': posts}
     return render(request, 'Home/profile.html', context)
-
 
 def search(request):
     query = request.GET['query']
