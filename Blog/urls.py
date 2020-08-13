e68c34dd9caf01e19180cce31298b85aae31749a
Deepjyoti De <deepjyotide13@gmail.com>
2020-08-13 16:55:30 +0530

    commit

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from Home import views
@@ -22,20 +21,4 @@ urlpatterns = [
     path('settings/', views.accountSettings, name="settings"),
     path('profile/<str:pk>/', views.profile, name="profile"),
     path('search', views.search, name="search"),
-    path('reset_password/',
-     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
-     name="reset_password"),
-
-    path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
-        name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
-     name="password_reset_confirm"),
-
-    path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
-        name="password_reset_complete"),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ToTell/settings.py
+++ ToTell/settings.py
@@ -132,3 +132,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
 
+# EMAIL
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bandeve.dad@gmail.com'
+EMAIL_HOST_PASSWORD = 'bandeve.django'
+
